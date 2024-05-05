bda340253d2838e1230d58f8018f1946e59a5340
juliosalvat <code@juliosalvat.com>
2024-05-05 15:58:29 -0700

    2024 update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const checkbox = document.getElementById('navi-toggle');
+const link = document.getElementById('myLink');
+
+link.addEventListener('click', function(event) {                
+    checkbox.checked = false;
+});
